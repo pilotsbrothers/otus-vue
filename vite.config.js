84282f71e96dcc77ecdceae572dfa6742e66a42f
KarpoviV <karpoviv@edu.mos.ru>
2023-11-19 08:45:55 +0300

    Revert "HW2"
    This reverts commit b9331aa005dbb3e00697dbfcfdaee70d7882968c.

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import ElementPlus from 'unplugin-element-plus/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(), ElementPlus()
+    vue(),
   ],
   resolve: {
     alias: {
