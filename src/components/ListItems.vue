b9331aa005dbb3e00697dbfcfdaee70d7882968c
KarpoviV <karpoviv@edu.mos.ru>
2023-11-19 08:45:29 +0300

    HW2

---
+++ src/components/ListItems.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <el-row>
+      <el-col :span="6"  v-for="item in data">
+        <item-element :item="item"></item-element>
+      </el-col>
+    </el-row>
+  </section>
+</template>
+
+<script setup>
+  import { ElCol, ElRow } from 'element-plus'
+  import ItemElement from "./ItemElement.vue";
+
+  defineProps(['data'])
+</script>
+
+<style scoped>
+
+</style>
